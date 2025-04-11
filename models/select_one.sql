d17ac83bd4fc0f58eb1f844ce1356185eda3cad3
Mikolaj Kupiec <mikolaj.kupiec@adverity.com>
2025-04-11 15:45:47 +0200

    Little change to have 10s transformation with logs

--- models/select_one.sql
+++ models/select_one.sql
@@ -1,1 +1,9 @@
-SELECT 1 AS id
+{% set iterations = range(5) %}
+
+{% for i in iterations %}
+    {{ log("🕒 Iteration " ~ i ~ ": sleeping 2 seconds", info=True) }}
+
+    call system$wait(2);
+{% endfor %}
+
+select 1 as id;
