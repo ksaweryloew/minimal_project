feee6386866917f3ef9c02575ea1a28020fb12cc
Mikolaj Kupiec <mikolaj.kupiec@adverity.com>
2025-04-25 00:27:34 +0200

    Removing sleep as it's not supported by redshift

--- models/select_one.sql
+++ models/select_one.sql
@@ -1,9 +1,1 @@
-{% set iterations = range(5) %}
-
-{% for i in iterations %}
-    {{ log("🕒 Iteration " ~ i ~ ": sleeping 2 seconds", info=True) }}
-
-    call system$wait(2);
-{% endfor %}
-
 select 1 as id;
